0e94191f1a710de2df67bf54b5f39fbc833d2ca5
William Wiltshire <william.wiltshire@hotmail.ca>
2015-03-20 13:14:37 -0400

    March 20th Commit
    Work done... today?

---
+++ TotalSquash/TotalSquash/TotalSquash/Controllers/LandingPageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TotalSquash.Models;
+
+namespace TotalSquash.Controllers
+{
+    public class LandingPageController : Controller
+    {
+        //
+        // GET: /LandingPage/
+        private PrimarySquashDBContext db = new PrimarySquashDBContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
