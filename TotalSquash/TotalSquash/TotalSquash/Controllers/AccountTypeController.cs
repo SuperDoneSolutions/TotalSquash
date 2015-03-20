74fe636a45a71f59982f8e7c745eb0bd245f0371
William Wiltshire <william.wiltshire@hotmail.ca>
2015-03-19 23:55:44 -0400

    TotalSquash - Initial Commit
    First Commit, filled provinces, countries, skills and account types

---
+++ TotalSquash/TotalSquash/TotalSquash/Controllers/AccountTypeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TotalSquash.Models;
+
+namespace TotalSquash.Controllers
+{
+    public class AccountTypeController : Controller
+    {
+        private PrimarySquashDBContext db = new PrimarySquashDBContext();
+
+        // GET: /AccountType/
+        public ActionResult Index()
+        {
+            return View(db.AccountTypes.ToList());
+        }
+
+        // GET: /AccountType/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountType accounttype = db.AccountTypes.Find(id);
+            if (accounttype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accounttype);
+        }
+
+        // GET: /AccountType/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /AccountType/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="accountId,description")] AccountType accounttype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AccountTypes.Add(accounttype);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(accounttype);
+        }
+
+        // GET: /AccountType/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountType accounttype = db.AccountTypes.Find(id);
+            if (accounttype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accounttype);
+        }
+
+        // POST: /AccountType/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="accountId,description")] AccountType accounttype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(accounttype).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(accounttype);
+        }
+
+        // GET: /AccountType/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AccountType accounttype = db.AccountTypes.Find(id);
+            if (accounttype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accounttype);
+        }
+
+        // POST: /AccountType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AccountType accounttype = db.AccountTypes.Find(id);
+            db.AccountTypes.Remove(accounttype);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TotalSquash/TotalSquash/TotalSquash/Controllers/SkillLevelController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TotalSquash.Models;
+
+namespace TotalSquash.Controllers
+{
+    public class SkillLevelController : Controller
+    {
+        private PrimarySquashDBContext db = new PrimarySquashDBContext();
+
+        // GET: /SkillLevel/
+        public ActionResult Index()
+        {
+            return View(db.Skills.ToList());
+        }
+
+        // GET: /SkillLevel/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Skill skill = db.Skills.Find(id);
+            if (skill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skill);
+        }
+
+        // GET: /SkillLevel/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /SkillLevel/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="skillId,description")] Skill skill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Skills.Add(skill);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(skill);
+        }
+
+        // GET: /SkillLevel/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Skill skill = db.Skills.Find(id);
+            if (skill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skill);
+        }
+
+        // POST: /SkillLevel/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="skillId,description")] Skill skill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(skill).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(skill);
+        }
+
+        // GET: /SkillLevel/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Skill skill = db.Skills.Find(id);
+            if (skill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skill);
+        }
+
+        // POST: /SkillLevel/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Skill skill = db.Skills.Find(id);
+            db.Skills.Remove(skill);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
